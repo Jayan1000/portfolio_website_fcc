3fc19afae2e91dfbaadd119e296efeea1c3a6f03
Jayan <jayangandhi7@gmail.com>
2022-07-27 13:46:04 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
+
+
+
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
+locations = []
+
 app = Flask(__name__)
 @app.route('/')
 def index():
+    first_name = request.args.get('first_name')
+    print(first_name)
+    
+    print(record)
+    print(record["key"])
+    print(type(record["key"]))
+    print(type(record["arr"]))
     return jsonify({'name': 'alice',
                     'email': 'alice@outlook.com'})
+            
+
+@app.route('/submit_contact_form', methods=['POST'])
+def submit_contact_form():
+    record = json.loads(request.data)
+    locations.append(record)
+    return ('suceuss')
+    
+@app.route('/get_map_locations', methods=['GET'])
+def get_map_locations():
+    return jsonify(locations)
+   
 
 app.run()
